c9e9b5b66833447ff760b056eb19847662a583a2
igor-devloper <wagnerigor9@gmail.com>
2023-04-20 15:29:07 -0300

    add new animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Atendimento } from './components/Atendimento'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
@@ -7,13 +7,28 @@ import { Sidebar } from './components/Sidebar'
 
 
 export function App() {
+  const [color, setColor] = useState(false)
+
   useEffect(() => {
     Aos.init({ duration: 1500 })
+  }, [])
+
+  useEffect(() => {
+    function positionScroll(){
+      if(window.scrollY > 500){
+        setColor(true)
+      }else {
+        setColor(false)
+      } 
+    }
+    
+
+    window.addEventListener('scroll', positionScroll)
   }, [])
   return (
     <div>
       <section className='flex h-[700px] bg-main' id='#home'>
-        <Sidebar/>
+        <Sidebar acao={color}/>
         <div className='max-w-md m-auto mt-32 flex-1'>
           <img src={bg} alt="" className='m-auto' />
         </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,47 @@
 import { CrownSimple, FacebookLogo, Headset, InstagramLogo, MapPin, Megaphone, Wrench } from "@phosphor-icons/react";
 import logo from '../assets/logo.png'
+import white from '../assets/whitw.png'
 
-export function Sidebar() {
+interface SidebarProps {
+  acao: boolean;
+}
+
+export function Sidebar(props: SidebarProps) {
   return (
-    <aside className='bg-white w-24 h-screen fixed'>
-      <img src={logo} alt="" className='max-h-8 flex m-auto mt-10' />
+    
+    <aside className={(props.acao ? 'bg-atendimento w-24 h-screen fixed transition-all' : 'bg-white w-24 h-screen fixed')}>
+
+      {props.acao ? <img src={white} alt="" className='max-h-8 flex m-auto mt-10' /> : <img src={logo} alt="" className='max-h-8 flex m-auto mt-10' />}
+
       <nav className='mt-28 p-6 space-y-4'>
         <a href="#home" className='flex items-center gap-2'>
-          <Wrench size={32} color="#192338" weight="bold" />
+          <Wrench size={32}  className={(props.acao ?'text-white' : '')}weight="bold" />
         </a>
         <a href="" className='flex items-center gap-2'>
-          <Headset size={32} color="#192338" weight="bold" />
+          <Headset size={32}  className={(props.acao ?'text-white' : 'b')}weight="bold" />
         </a>
         <a href="" className='flex items-center gap-2'>
-          <CrownSimple size={32} color="#192338" weight="bold" />
+          <CrownSimple size={32}  className={(props.acao ?'text-white' : '')}weight="bold" />
         </a>
         <a href="" className='flex items-center gap-2'>
-          <Megaphone size={32} color="#192338" weight="bold" />
+          <Megaphone size={32}  className={(props.acao ? 'text-white' : '')}weight="bold" />
         </a>
         <a href="" className='flex items-center gap-2'>
-          <MapPin size={32} color="#192338" weight="bold" />
+          <MapPin size={32} weight="bold" className={(props.acao ? 'text-white' : '')}/>
         </a>
       </nav>
+
+
       <div className='p-6 space-y-4 mt-10'>
-        <a href="" className='flex items-center gap-2'>
-          <InstagramLogo size={42} color="#386BA1" />
+        <a href="" className={props.acao ? 'text-white flex items-center gap-2' : 'flex items-center gap-2'}>
+          <InstagramLogo size={42}/>
         </a>
-        <a href="" className='flex items-center gap-2'>
-          <FacebookLogo size={42} color="#386BA1" />
+        <a href="" className={props.acao ? 'text-white flex items-center gap-2' : 'flex items-center gap-2'}>
+          <FacebookLogo size={42} />
         </a>
       </div>
+
+
     </aside>
   )
 }
